7e66e2e8cdbf428559f155fd2f9b8a6dc83ac65d
Cody Boppert <cboppert@gmail.com>
2018-10-23 02:07:54 -0400

    Add gender analysis module

--- src/gender_analysis.erl
+++ src/gender_analysis.erl
@@ -1,4 +1,25 @@
 -module(gender_analysis).
 -export([analyze/1]).
 
-analyze(Text) -> Text.
+-define(HePattern, "\\b[H,h]e\\b").
+-define(ShePattern, "\\b[S,s]he\\b").
+
+analyze(Text) ->
+   case {check_if_male(Text), check_if_female(Text)} of
+      {true, true} -> unknown;
+      {true, false} -> male;
+      {false, true} -> female;
+      {false, false} -> unknown
+   end.
+
+check_if_male(Text) ->
+   check_gender(Text, ?HePattern).
+
+check_if_female(Text) ->
+   check_gender(Text, ?ShePattern).
+
+check_gender(Text, Pattern) ->
+   case re:run(Text, Pattern) of
+      {match, _} -> true;
+      nomatch -> false
+   end.
